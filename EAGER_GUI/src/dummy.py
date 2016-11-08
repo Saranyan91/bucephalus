5f0f055897f88f8a62885f10edbaafb39c7bce6b
Saranyan91 <saran.s6691@gmail.com>
2016-11-08 13:15:03 -0600

    removing files

--- EAGER_GUI/src/dummy.py
+++
@@ -1,6 +0,0 @@
-import sys
-import os
-import os.path
-id=123
-name="Jonathan Grimm"
-os.system(sys.executable+" "+os.path.dirname(__file__)+"\..\src-security\LoginModule.py"+" "+str(id)+' "'+name+'"');
